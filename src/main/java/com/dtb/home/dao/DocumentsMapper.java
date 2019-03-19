c9ada22c3a6fa5b69ca48f779b69c34efef61f61
lmx <datiba_admin@163.com>
2019-03-20 01:12:13 +0800

    '完成了文档列表查询页面的基本实现，搜索暂未实现，页面遗留有bug，下拉菜单和vue冲突，还需要添加评分和下载次数标签'

--- src/main/java/com/dtb/home/dao/DocumentsMapper.java
+++ src/main/java/com/dtb/home/dao/DocumentsMapper.java
@@ -1,6 +1,8 @@
 package com.dtb.home.dao;
 
 import com.dtb.entity.Documents;
+import com.dtb.entity.DocumentsAssociation;
+import com.github.pagehelper.Page;
 import org.apache.ibatis.annotations.Param;
 
 public interface DocumentsMapper {
@@ -14,6 +16,15 @@ public interface DocumentsMapper {
      * @return int受影响行数
      */
     int insertSelective(Documents record);
+
+    /**
+     * @auther lmx
+     * @date 2019/3/19 22:45
+     * @descript 分页查询文件列表
+     * @param documents
+     * @return com.github.pagehelper.Page<com.dtb.entity.DocumentsAssociation>
+     */
+    Page<DocumentsAssociation> selectDocumentListToLimit(Documents documents);
 
     Documents selectByPrimaryKey(@Param("id") Integer id);
 
--- src/main/java/com/dtb/home/dao/UserMapper.java
+++ src/main/java/com/dtb/home/dao/UserMapper.java
@@ -87,7 +87,7 @@ public interface UserMapper {
 
     int insertSelective(User record);
 
-    User selectByPrimaryKey(Integer id);
+    User selectByPrimaryKey(@Param("id") Integer id);
 
     int updateByPrimaryKeySelective(User record);
 
